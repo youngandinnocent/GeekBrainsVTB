8fe24423388778a5d8c8da1e5c3da6b0443b4f5a
etitaeva <ekaterina.titaeva@gmail.com>
2020-03-03 21:59:55 +0300

    fix 9 task

--- ekaterina_titaeva/9/js/script.js
+++ ekaterina_titaeva/9/js/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('cvs');
 const context = canvas.getContext("2d");
-const canvasWidth = canvas.getAttribute('width');
+const canvasWidth = parseInt(canvas.getAttribute('width'));
 const cardWidth = 150;
 const cardHeight = 30;
 
@@ -17,13 +17,14 @@ class Person {
     }
 }
 
-function paint(node, x = canvasWidth / 2, y = 10, xParent = 0) {
+function paint(node, x = canvasWidth / 2, y = 10, startArea = 0, endArea = canvasWidth) {
     context.fillText(`${node.name} ${node.lastName}, ${node.age}`, x - cardWidth / 2 + 10, y + cardHeight / 2);
     context.strokeRect(x - cardWidth / 2, y, cardWidth, cardHeight);
     for (let i = 0; i < node.friends.length; i++) {
-        let areaWidth = (xParent - x) ? (Math.abs(xParent - x) * 2) : (x * 2);
-        let xChild = areaWidth / node.friends.length / 2 + areaWidth / node.friends.length * i;
-        if (x > 500) xChild += 500;
+        let areaWidth = (endArea - startArea);
+        let leftBorder = startArea + areaWidth / node.friends.length * i;
+        let rigthBorder = startArea + areaWidth / node.friends.length * (i + 1);
+        let xChild = leftBorder + areaWidth / node.friends.length / 2;
         let yChild = y + 40;
 
         context.beginPath();
@@ -31,7 +32,7 @@ function paint(node, x = canvasWidth / 2, y = 10, xParent = 0) {
         context.lineTo(xChild, yChild);
         context.stroke();
 
-        paint(node.friends[i], xChild, yChild, x);
+        paint(node.friends[i], xChild, yChild, leftBorder, rigthBorder);
     }
 }
 
