ae955c7f71c3661fa1bb8dc43242863e6bb4436a
acmeCorpsRF <acme.corps.rf@gmail.com>
2020-03-13 12:04:56 +0300

    13.03

---
+++ Ivanov Nikolai/lesson13-React/src/components/Message/Message.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Message.css';
+import PropTypes from 'prop-types';
+
+export class Message extends Component {
+
+    static propTypes = {
+        message: PropTypes.object.isRequired
+    };
+
+    render() {
+        return <li className={'message '+ Object.keys(this.props.message)}>
+            {Object.values(this.props.message)}
+        </li>
+    }
+
+}
