d3351f5c8816e8cb79a59503cab53ca7d6f7b253
Nikita Stavitskii <nstavickii@vtb.ru>
2020-03-17 23:33:15 +0300

    Edited Header component and styles

--- Stavitskii_Nikita/14/src/components/Header/Header.jsx
+++ Stavitskii_Nikita/14/src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import './Header.css';
 export class Header extends Component {
     render() {
         return (
-            <div className="header"></div>
+            <div className="header">
+                <div className="header-list">Some logo or search</div>
+                <div className="header-chat">Some chat name</div>
+            </div>
         )
     }
 }
