52f9c8187cea08b924b3ece6f0e295f8d4bced9c
Millerqwe <millerqwe@gmail.com>
2020-03-17 18:59:07 +0300

    Add homework from 14 lesson [melnikov]

---
+++ melnikov_dmitrii/Lesson 14/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'App.js'),
+    output: {
+        path: path.resolve(__dirname, 'dict'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node modules/,
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]        
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filiname: 'main.css'
+        })
+    ]
+}
